2f038f1627da39ee6383855ac79d2d68a939c5c7
Sarvenaz2000 <SARVENAZashoori2000@GMAIL.COM>
2025-01-13 10:16:44 +0100

    Update distance_node.cpp

--- src/distance_node.cpp
+++ src/distance_node.cpp
@@ -4,6 +4,8 @@
 #include <turtlesim/Pose.h>
 #include <assignment1_rt/distance.h>  // Include the custom message for distance
 #include <cmath>
+#include <vector>
+#include <algorithm>
 
 // Declare global publishers and position variables
 ros::Publisher pub_turtle1;
@@ -14,7 +16,11 @@ ros::Publisher pub_distance;
 float turtle1_x, turtle1_y;
 
 // Distance threshold for stopping turtles
-float threshold = 1.0;
+float distance_threshold = 1.0;
+float obstacle_threshold = 0.5;  // Threshold distance to obstacles
+
+// Store obstacle distances
+std::vector<double> obstacle_distances(16, std::numeric_limits<double>::infinity());
 
 void turtle1PoseCallback(const turtlesim::Pose::ConstPtr& msg) {
     // Store the position of turtle1
@@ -33,37 +39,48 @@ void turtle2PoseCallback(const turtlesim::Pose::ConstPtr& msg) {
     // Publish the distance to a custom message type
     assignment1_rt::distance dist_msg;
     dist_msg.distance = distance;
-
-    // Publish the custom distance message to the /turtle_distance_topic
     pub_distance.publish(dist_msg);
 
-    // Check if turtles are too close or near boundaries
-    if (distance < threshold || x2 > 10.0 || y2 > 10.0 || x2 < 1.0 || y2 < 1.0) {
+    // Check for obstacle proximity
+    if (*std::min_element(obstacle_distances.begin(), obstacle_distances.end()) < obstacle_threshold) {
+        ROS_WARN("Stopping turtle2 due to nearby obstacle.");
         geometry_msgs::Twist stop_msg;
-        pub_turtle1.publish(stop_msg);  // Stop turtle1
-        pub_turtle2.publish(stop_msg);  // Stop turtle2
-        ROS_WARN("Turtles are too close or near boundaries. Stopping.");
+        stop_msg.linear.x = 0.0;
+        stop_msg.angular.z = 0.0;
+        pub_turtle2.publish(stop_msg);
+        return;
+    }
+
+    // Default behavior if no obstacle is too close
+    geometry_msgs::Twist move_msg;
+    move_msg.linear.x = (distance > distance_threshold) ? 1.0 : 0.0;
+    move_msg.angular.z = 0.0;
+    pub_turtle2.publish(move_msg);
+}
+
+void obstaclesCallback(const std_msgs::Float32MultiArray::ConstPtr& msg) {
+    // Update obstacle distances
+    if (msg->data.size() == 16) {
+        obstacle_distances = msg->data;
+    } else {
+        ROS_ERROR("Received obstacle data of unexpected size.");
     }
 }
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "distance_node");
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "distance_node_with_obstacles");
     ros::NodeHandle nh;
 
-    // Subscribers to get poses of turtle1 and turtle2
-    ros::Subscriber sub_turtle1 = nh.subscribe("/turtle1/pose", 10, turtle1PoseCallback);
-    ros::Subscriber sub_turtle2 = nh.subscribe("/turtle2/pose", 10, turtle2PoseCallback);
-
-    // Publishers for controlling turtle velocities
+    // Publishers
     pub_turtle1 = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
     pub_turtle2 = nh.advertise<geometry_msgs::Twist>("/turtle2/cmd_vel", 10);
+    pub_distance = nh.advertise<assignment1_rt::distance>("/distance", 10);
 
-    // Publisher for publishing custom distance message (distance)
-    pub_distance = nh.advertise<assignment1_rt::distance>("distance_topic", 10);
+    // Subscribers
+    ros::Subscriber sub_turtle1 = nh.subscribe("/turtle1/pose", 10, turtle1PoseCallback);
+    ros::Subscriber sub_turtle2 = nh.subscribe("/turtle2/pose", 10, turtle2PoseCallback);
+    ros::Subscriber sub_obstacles = nh.subscribe("/obstacles", 10, obstaclesCallback);
 
-    // Spin to keep the node active
     ros::spin();
-
     return 0;
 }
-
